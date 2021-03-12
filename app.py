a678ff2ffa92040591d51abb7b2bf93a7d7ff2db
ashagm <ashaagm@gmail.com>
2021-03-11 21:15:01 -0600

    saving the qualified loans into csv file

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ import fire
 import questionary
 from pathlib import Path
 
-from qualifier.utils.fileio import load_csv
+from qualifier.utils.fileio import (
+    load_csv, 
+    save_csv,
+)
 
 from qualifier.utils.calculators import (
     calculate_monthly_debt_ratio,
@@ -110,6 +113,15 @@ def save_qualifying_loans(qualifying_loans):
     """
     # @TODO: Complete the usability dialog for savings the CSV Files.
     # YOUR CODE HERE!
+
+    print(f"qualifying loans is {qualifying_loans}")
+
+    csvpath = questionary.text("Enter a file path to save into (.csv):").ask()
+    csvpath = Path(csvpath)
+    if not csvpath.exists():
+        sys.exit(f"Oops! Can't find this path: {csvpath}")
+
+    return save_csv(csvpath, qualifying_loans)
 
 
 def run():
